342166536f8eb9477f23d9ea54f55caf857d7080
UshaSah <72180046+UshaSah@users.noreply.github.com>
2024-03-19 11:29:59 -0700

    Add files via upload

---
+++ ResearchRoute/ResearchRoute/ResearchRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchRouteApp.swift
+//  ResearchRoute
+//
+//  Created by Mac on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ResearchRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
