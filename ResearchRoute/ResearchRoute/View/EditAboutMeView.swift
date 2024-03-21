6b95f3c6a3615c3101853071fb44493266b20fe9
Tyler Tran <104945572+tylertran349@users.noreply.github.com>
2024-03-21 10:29:08 -0700

    Add logged in user retrieval

--- ResearchRoute/ResearchRoute/View/EditAboutMeView.swift
+++ ResearchRoute/ResearchRoute/View/EditAboutMeView.swift
@@ -1,18 +1,64 @@
 //
 //  EditAboutMeView.swift
-//  ResearchRoute
+//  Project
 //
-//  Created by apple on 3/20/24.
+//  Created by apple on 2/29/24.
 //
 
 import SwiftUI
 
 struct EditAboutMeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @State var text: String = "Existing text here"
+    
+    let titleFontName: String = "Poppins-Bold"
+    let subtitleFontName: String = "Poppins-SemiBold"
+    let bodyFontName: String = "Poppins-Regular"
 
-#Preview {
-    EditAboutMeView()
+    let titleFontSize: CGFloat = 24
+    let subtitleFontSize: CGFloat = 18
+    let bodyFontSize: CGFloat = 12
+
+    let titleColor: Color = Color(red: 1/255, green: 159/255, blue: 171/255)
+    let subtitleColor: Color = Color(red: 58/255, green: 58/255, blue: 62/255)
+    let bodyColor: Color = Color(red: 129/255, green: 136/255, blue: 152/255)
+
+    let skillsArray = ["HTML", "CSS", "JavaScript", "React.js", "MongoDB", "Swift", "Angular.js", "Python"]
+    let coursesArray = ["Data Structures and Algorithms", "Operating Systems", "Artificial Intelligence", "Computer Vision", "iOS App Development"]
+    
+    @State private var statusMessage: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 15) {
+                Text("Edit About Me")
+                    .font(.custom(titleFontName, size: titleFontSize))
+                    .foregroundStyle(titleColor)
+                    .multilineTextAlignment(.center)
+                
+                VStack {
+                    TextField(text, text: $text, axis: .vertical)
+                        .font(.custom(bodyFontName, size: subtitleFontSize))
+                        .foregroundStyle(bodyColor)
+                    Divider()
+                }
+                
+                Button(action: {
+                    print("Saved")
+                    statusMessage = "Your changes have been successfully saved"
+                }) {
+                    Text("Save")
+                        .font(.custom(subtitleFontName, size: subtitleFontSize))
+                        .padding(.vertical, 5)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .foregroundStyle(.white)
+                        .background(titleColor)
+                        .cornerRadius(5)
+                }
+                
+                Text(statusMessage)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding(30)
+        }
+    }
 }
